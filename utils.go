7b89db978f598df8fe241db4c86c49dd3e93fd5c
it512 <it512@msn.com>
2025-07-12 15:37:27 +0800

    rename

---
+++ utils.go
@@ -0,0 +1,22 @@
+package xxl
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func jsonTo(code int, resp any, w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	enc := json.NewEncoder(w)
+	return enc.Encode(resp)
+}
+
+func bind(r io.Reader, p any) error {
+	dec := json.NewDecoder(r)
+	if err := dec.Decode(p); err != nil {
+		return err
+	}
+	return nil
+}
