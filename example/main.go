4225d9845ea0e3f159a391a4c47c0ca54992ee5f
it512 <it512@msn.com>
2025-07-12 13:57:50 +0800

    example

---
+++ example/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/it512/xxl-job-exec"
+)
+
+func main() {
+	exec := xxl.NewExecutor(
+		xxl.ServerAddr("http://127.0.0.1:8080/xxl-job-admin"),
+		xxl.AccessToken("default_token"),
+		xxl.RegistryKey("test"),
+		xxl.ExecutorURL("http://127.0.0.1:10010"),
+	)
+
+	exec.RegTask("test", func(ctx context.Context, task *xxl.Task) error {
+		fmt.Println("OK")
+		return nil
+	})
+
+	exec.RegTask("ok", func(ctx context.Context, task *xxl.Task) error {
+		fmt.Println("OK")
+		return nil
+	})
+
+	exec.RegTask("error", func(ctx context.Context, task *xxl.Task) error {
+		return errors.New("this is a error")
+	})
+
+	exec.RegTask("panic", func(ctx context.Context, task *xxl.Task) error {
+		panic("this is a panic")
+	})
+
+	exec.RegTask("timeout", func(ctx context.Context, task *xxl.Task) error {
+		<-ctx.Done()
+		fmt.Println("timeout or killed!")
+		return nil
+	})
+
+	exec.Start()
+	defer exec.Stop()
+
+	http.ListenAndServe(":10010", exec.Handle())
+}
