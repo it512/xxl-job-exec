e56215d8882cb18b57c918acc796120aa6547c89
it512 <it512@msn.com>
2025-07-13 08:02:49 +0800

    chi

---
+++ example/go.mod
@@ -0,0 +1,10 @@
+module example
+
+go 1.24.2
+
+require (
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/it512/xxl-job-exec v0.0.0
+)
+
+replace github.com/it512/xxl-job-exec => ../
--- example/main.go
+++ example/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/it512/xxl-job-exec"
 )
 
@@ -14,7 +15,7 @@ func main() {
 		xxl.ServerAddr("http://127.0.0.1:8080/xxl-job-admin"),
 		xxl.AccessToken("default_token"),
 		xxl.RegistryKey("test"),
-		xxl.ExecutorURL("http://127.0.0.1:10010"),
+		xxl.ExecutorURL("http://127.0.0.1:10010/xxl-job"),
 	)
 
 	exec.RegTask("test", func(ctx context.Context, task *xxl.Task) error {
@@ -44,5 +45,9 @@ func main() {
 	exec.Start()
 	defer exec.Stop()
 
-	http.ListenAndServe(":10010", exec.Handle())
+	mux := chi.NewMux()
+
+	mux.Mount("/xxl-job", exec.Handle())
+
+	http.ListenAndServe(":10010", mux)
 }
